46fd610a91e475e4ca66edc89b7bf82d2385dd67
Upendra <upendra@DES000447s-iMac.local>
2019-12-23 15:28:17 +0530

    first commit

---
+++ data/sampleData.js
@@ -0,0 +1,238 @@
+var data =[
+    
+    {
+      "target": "president",
+      "value": 235,
+      "source": "cnndebate"
+    },
+    {
+      "target": "snl",
+      "value": 238,
+      "source": "donaldtrump"
+    },
+    {
+      "target": "losers",
+      "value": 240,
+      "source": "cnndebate"
+    },
+    {
+      "target": "obama",
+      "value": 241,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "equalpay",
+      "value": 242,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "republicans",
+      "value": 245,
+      "source": "democrat"
+    },
+    {
+      "target": "obama",
+      "value": 246,
+      "source": "democrat"
+    },
+    {
+      "target": "gop",
+      "value": 247,
+      "source": "democrat"
+    },
+    {
+      "target": "democrats",
+      "value": 250,
+      "source": "cnndebate"
+    },
+    {
+      "target": "#cnndebate",
+      "value": 250,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "woman",
+      "value": 250,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "demdebate",
+      "value": 260,
+      "source": "joebiden"
+    },
+    {
+      "target": "socialist",
+      "value": 264,
+      "source": "berniesanders"
+    },
+    {
+      "target": "democratic",
+      "value": 275,
+      "source": "berniesanders"
+    },
+    {
+      "target": "good",
+      "value": 277,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "democraticdebate",
+      "value": 279,
+      "source": "donaldtrump"
+    },
+    {
+      "target": "president",
+      "value": 283,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "vote",
+      "value": 297,
+      "source": "berniesanders"
+    },
+    {
+      "target": "email",
+      "value": 298,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "makeamericagreatagain",
+      "value": 301,
+      "source": "donaldtrump"
+    },
+    {
+      "target": "achievement",
+      "value": 310,
+      "source": "cnndebate"
+    },
+    {
+      "target": "obama",
+      "value": 315,
+      "source": "donaldtrump"
+    },
+    {
+      "target": "people",
+      "value": 319,
+      "source": "berniesanders"
+    },
+    {
+      "target": "revolution",
+      "value": 321,
+      "source": "berniesanders"
+    },
+    {
+      "target": "whoa",
+      "value": 339,
+      "source": "berniesanders"
+    },
+    {
+      "target": "benghazi",
+      "value": 342,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "cnn",
+      "value": 373,
+      "source": "berniesanders"
+    },
+    {
+      "target": "hillary",
+      "value": 433,
+      "source": "berniesanders"
+    },
+    {
+      "target": "martinomalley",
+      "value": 456,
+      "source": "cnndebate"
+    },
+    {
+      "target": "#berniesanders",
+      "value": 833,
+      "source": "cnndebate"
+    },
+    {
+      "target": "#donaldtrump",
+      "value": 836,
+      "source": "cnndebate"
+    },
+    {
+      "target": "bernie",
+      "value": 943,
+      "source": "cnndebate"
+    },
+    {
+      "target": "bury",
+      "value": 946,
+      "source": "cnndebate"
+    },
+    {
+      "target": "libya",
+      "value": 947,
+      "source": "cnndebate"
+    },
+    {
+      "target": "hillaryemail",
+      "value": 958,
+      "source": "cnndebate"
+    },
+    {
+      "target": "demdebate",
+      "value": 1045,
+      "source": "democrat"
+    },
+    {
+      "target": "#berniesanders",
+      "value": 1053,
+      "source": "hillaryclinton"
+    },
+    {
+      "target": "benghazi",
+      "value": 1102,
+      "source": "cnndebate"
+    },
+    {
+      "target": "realdonaldtrump",
+      "value": 1823,
+      "source": "cnndebate"
+    },
+    {
+      "target": "#hillaryclinton",
+      "value": 2420,
+      "source": "cnndebate"
+    }
+  ]
+
+// var data = [
+    
+//     {
+//         "source": "Aneesha",
+//         "value": 100,
+//         "target": "Maneesha"
+//     },
+//     {
+//         "source": "Upendra",
+//         "value": 200,
+//         "target": "Deepak"
+//     },
+//     {
+//         "source": "Hilary",
+//         "value": 300,
+//         "target": "Deepak"
+//     },
+
+//     {
+//         "source": "Upendra",
+//         "value": 400,
+//         "target": "Dhirishiya"
+//     },
+//     {
+//         "source": "Vinoth",
+//         "value": 500,
+//         "target": "Dhirishiya"
+//     },
+//     {
+//         "source": "Kitty",
+//         "value": 800,
+//         "target": "All of the above"
+//     }
+// ]
